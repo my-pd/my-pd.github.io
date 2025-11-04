6793175151c4465010f18eff2032a1b775d347fe
my-pd <csm.mohasin.pd@gmail.com>
2025-11-04 08:16:58 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkr6vjrbyKn6aUJZKCsb4Qp-uHIglplY8",
+  authDomain: "my-private-drive.firebaseapp.com",
+  projectId: "my-private-drive",
+  storageBucket: "my-private-drive.firebasestorage.app",
+  messagingSenderId: "316857258267",
+  appId: "1:316857258267:web:06a0aa79baeaccfeefc3be",
+  measurementId: "G-1FLQ460XFT"
+};
